6a33533ec0d96dbc889982718454513b2f8f3eee
fh3_23 <fh3@usd348.com>
2014-09-23 14:49:59 -0500

    September 23, 2014 2:49 PM Application has just turned into a game

--- flaming-octo-ironman/src/com/flamingOctoIronman/Timer.java
+++ flaming-octo-ironman/src/com/flamingOctoIronman/Timer.java
@@ -10,15 +10,49 @@ import com.flamingOctoIronman.events.CoreEventHandler;
  *
  */
 public class Timer {
-	private static int tickCount = 0;
+	private static float tickCount = 0;
+	private float passedTicks;
+	public Timer(){
+		setCurrentTick();
+	}
 	
+	/**
+	 * Called on every tick, increase the tick count
+	 */
 	@CoreEventHandler(event = "GameLoopEvent")
 	public static void tickEvent(){
 		tickCount++;
-		System.out.println(tickCount);
+		System.out.println(String.format("Tick: %.0f", tickCount));
 	}
 	
-	public static int getTickCount(){
+	/**
+	 * Returns the current number of ticks that have passed since the start of the game
+	 * @return Ticks passed
+	 */
+	public static float getTickCount(){
 		return tickCount;
+	}
+	
+	/**
+	 * Returns the number of ticks that have passed since the base tick count was set
+	 * @return Ticks passed
+	 */
+	public float ticksPassed(){
+		return tickCount - passedTicks;
+	}
+	
+	/**
+	 * Sets the base tick count to the current tick
+	 */
+	public void setCurrentTick(){
+		passedTicks = tickCount;
+	}
+	
+	/**
+	 * Sets the base number of ticks to the given tick number
+	 * @param Tick to set the base tick count to
+	 */
+	public void setCurrentTick(float tick){
+		passedTicks = tick;
 	}
 }
