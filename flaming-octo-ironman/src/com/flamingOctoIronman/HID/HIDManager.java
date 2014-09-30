fbbc19264a1eb5962bc73fba8a5ccfca1cd0e804
fh3_23 <fh3@usd348.com>
2014-09-30 14:37:24 -0500

    Did a little HID stuff

--- flaming-octo-ironman/src/com/flamingOctoIronman/HID/HIDManager.java
+++ flaming-octo-ironman/src/com/flamingOctoIronman/HID/HIDManager.java
@@ -5,6 +5,11 @@ package com.flamingOctoIronman.HID;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 import javax.swing.JFrame;
 
@@ -17,20 +22,32 @@ import com.flamingOctoIronman.HID.inputEvents.KeyTypedEvent;
  * @author Quint Heinecke
  *
  */
-public class HIDManager implements KeyListener{
-	private InputEventBusService inputEventBus;
-	private InputMapper keyMap;
+//TODO Note to self: may need to rework this due to AWT threading model.
+public class HIDManager implements KeyListener, MouseListener, MouseWheelListener, MouseMotionListener{
+	private InputEventBusService inputEventBus;	//Hold and load all of the input events
+	private InputMapper keyMap;	//Map the keys to a function
 	
+	/**
+	 * Constructs a new instance of HIDManager
+	 */
 	public HIDManager(){
 		inputEventBus = new InputEventBusService();
 		keyMap = new InputMapper();
 	}
 	
+	/**
+	 * Registers the <code>JFrame frame</code> for key listening
+	 * @param frame The frame to register
+	 */
 	public void registerFrame(JFrame frame){
 		frame.addKeyListener(this);
 		frame.setFocusTraversalKeysEnabled(false);
 	}
 	
+	/**
+	 * Subscribes an object to input events
+	 * @param subscriber The object to subscribe
+	 */
 	public void subscribeInput(Object subscriber){
 		inputEventBus.subscribe(subscriber);
 	}
@@ -50,7 +67,69 @@ public class HIDManager implements KeyListener{
 		this.keyMap.putPressMap(e.getExtendedKeyCode(), false);
 	}
 	
+	/**
+	 * Checks to see if the key associated with the given <code>String</code> is pressed
+	 * @param UID The <code>String</code> to check
+	 * @return True if the key is pressed, false otherwise
+	 */
 	public boolean isPressed(String UID){
 		return this.keyMap.isPressed(UID);
+	}
+	
+	/**
+	 * Adds a <code>String</code>/<code>Key</code> pair to the key map
+	 * @param UID
+	 * @param Key
+	 */
+	public void putKeyMap(String UID, int Key){
+		this.keyMap.putKeyMap(UID, Key);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
