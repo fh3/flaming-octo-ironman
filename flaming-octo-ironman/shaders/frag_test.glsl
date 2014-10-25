b6df30ef456a1b82cdfff69d5927bfd1f154d0dd
fh3 <fh3@usd348.com>
2014-10-24 20:56:44 -0500

    Linear interpolation via LERP

--- flaming-octo-ironman/shaders/frag_test.glsl
+++ flaming-octo-ironman/shaders/frag_test.glsl
@@ -1,7 +1,12 @@
 #version 330
 
-out vec4 outputColor;
+out vec4 outputColor;	//Default out variable
 void main()
 {
-   outputColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);	//Set the output color
+	float lerpValue = gl_FragCoord.y / 600.0f; //Divide the y coord by 600, store the result in a new float value
+	outputColor = mix(//Mix function performs linear interpolation
+						vec4(1.0f, 1.0f, 1.0f, 1.0f),	//Point A
+						vec4(0.2f, 0.2f, 0.2f, 1.0f),	//Point B
+						lerpValue	//How far between the two points (must be between [0, 1]
+						);
 }
