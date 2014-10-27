74f5c0fdb03a2b1f617bfa04a9898aa9bdf9f1c4
fh3 <fh3@usd348.com>
2014-10-26 21:22:17 -0500

    Last commit for the weekend

--- flaming-octo-ironman/shaders/frag_test.glsl
+++ flaming-octo-ironman/shaders/frag_test.glsl
@@ -1,12 +1,10 @@
 #version 330
 
+smooth in vec4 theColor;	//New input variable, mast have same name
+
 out vec4 outputColor;	//Default out variable
+
 void main()
 {
-	float lerpValue = gl_FragCoord.y / 600.0f; //Divide the y coord by 600, store the result in a new float value
-	outputColor = mix(//Mix function performs linear interpolation
-						vec4(1.0f, 1.0f, 1.0f, 1.0f),	//Point A
-						vec4(0.2f, 0.2f, 0.2f, 1.0f),	//Point B
-						lerpValue	//How far between the two points (must be between [0, 1]
-						);
+	outputColor = theColor;
 }
--- flaming-octo-ironman/shaders/vertex_test.glsl
+++ flaming-octo-ironman/shaders/vertex_test.glsl
@@ -1,8 +1,13 @@
 #version 330
 
-layout(location = 0) in vec4 position;	//The input variable
+layout(location = 0) in vec4 position;	//The position
+layout(location = 1) in vec4 color;		//The color variable
+
 out vec4 gl_Position;	//The standard out variable
+smooth out vec4 theColor;	//A new output variable
+
 void main()
 {
     gl_Position = position;	//Set the output value to the input value
+    theColor = color;
 }
